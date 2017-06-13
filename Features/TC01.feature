b41d971aa3460f3a6509becd45bc2836e1d58424
LIEUTRAN <bichlieu206@gmail.com>
2017-06-13 20:33:44 +0700

    Push code first time

---
+++ Features/TC01.feature
@@ -0,0 +1,12 @@
+﻿Feature: TC01
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@invalid
+Scenario: Login with information invalid
+ Given I am on LiveGuru site
+ And I input username daominhdam123123@gmail.com and password 123123
+ When I click Login button
+ Then The error message should be displayed
+ And I quit browser
---
+++ Steps/TC_01_Steps.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System;
+using BDD_SPECFLOW_DEMO.UIs;
+using TechTalk.SpecFlow;
+
+namespace BDD_SPECFLOW_DEMO.Steps
+{
+    [Binding]
+    public class TC_01_Steps
+    {
+
+        private IWebDriver driver;
+
+        [Given(@"I am on LiveGuru site")]
+        public void GivenIAmOnLiveGuruSite()
+        {
+            driver = new ChromeDriver(@"G:\EXCERCISE\BDD_SPECFLOW_DEMO\BDD_SPECFLOW_DEMO\BDD_SPECFLOW_DEMO\Drivers");
+            driver.Navigate().GoToUrl(MyAccountUI.MY_ACCOUNT_URL);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+        }
+        
+        [Given(@"I input username (.*) and password (.*)")]
+        public void GivenIInputUsernameAndPassword(String userName, String passWord)
+        {
+            driver.FindElement(By.XPath(MyAccountUI.USERNAME_TXT)).SendKeys(userName);
+            driver.FindElement(By.XPath(MyAccountUI.PASSWORD_TXT)).SendKeys(passWord);
+        }
+        
+        [When(@"I click Login button")]
+        public void WhenIClickLoginButton()
+        {
+            driver.FindElement(By.XPath(MyAccountUI.LOGIN_BTN)).Click();
+        }
+        
+        [Then(@"The error message should be displayed")]
+        public Boolean ThenTheErrorMessageShouldBeDisplayed()
+        {
+            IWebElement errorMessage = driver.FindElement(By.XPath(MyAccountUI.INVALID_ERROR_MSG));
+            return errorMessage.Displayed;
+
+        }
+
+        [Then(@"I quit browser")]
+        public void ThenIQuitBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
